a0d03c1f992a91586935d08e0b444019da77d707
Kelvin Foster <98378771+fosterkelvin@users.noreply.github.com>
2022-05-13 17:55:52 +0800

    Add files via upload

---
+++ Exercise15/styles/Exercise15.js
@@ -0,0 +1,25 @@
+function validateForm() {
+    let fn = document.forms["form"]["fn"].value;
+    if (fn == "") {
+        alert("Name must be filled");
+        return false;
+    }
+}
+let fn='Kelvin Foster';
+    console.log("First Name: " + fn);
+let age = '19';
+    console.log("Age: " + age);
+let pn = '09515427238';
+    console.log("Phone Number: " + pn);
+let ea = '20197992@s.ubaguio.edu';
+    console.log("Email Address: " + ea);
+let ma = 'Tuding, Itogon, Benguet';
+    console.log("Mailing Address: " + ma);
+let pg = 'Computer Science';
+    console.log("Program: " + pg);
+let ID = '20197992';
+    console.log("ID number: " + ID);
+let yl = '1st Year';
+    console.log("Year Level: " + yl);
+let sec = 'IAA1';
+    console.log("Section: " + sec);
